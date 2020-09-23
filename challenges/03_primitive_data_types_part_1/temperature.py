63e74fe795c29d95f3eddd203c145dcd1ef80e90
NaiMedia <nailah_codes@yahoo.com>
2020-09-22 19:24:00 -0700

    adding temperature.py challenge

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,19 @@
+# this is how to convert 100 celsius to degrees fahrenheit
+celsius_100 = (100 - 32) * (5/9)
+print(celsius_100)
+
+# this is how to convert  0 celsius to degrees fahrenheit
+celsius_0 = (0 -32) * (5/9)
+print(celsius_0)
+
+# this is how to convert 34.2 degrees fahrenheit to celsius
+print((34.2- 32) * (5/9)) 
+
+# this is how to convert 5 celsius to degrees fahrenheit
+celsius = 5
+fahrenheit_5 = (9/5) * (celsius+32)
+print(fahrenheit_5) 
+
+# this was to determine if 30.2 celsius was hotter than 85.1 degrees fahrenheit
+celsius_30_2 = (30.2 - 32) * (5/9)
+print(celsius_30_2)
